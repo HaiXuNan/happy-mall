cce2b45dc9a50e09384d58cb621e98a7b200e3df
xuhainan <859493231@qq.com>
2020-07-15 22:38:57 +0800

    init 商家 商品服务

---
+++ happy-mall-commodity-service/src/main/java/top/mall/happy/commodity/HappyMallCommodityServiceApplication.java
@@ -0,0 +1,13 @@
+package top.mall.happy.commodity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyMallCommodityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyMallCommodityServiceApplication.class, args);
+    }
+
+}
