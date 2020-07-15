c7904d2cfc83633fd0ad59c4249e84b9635cafb7
xuhainan <859493231@qq.com>
2020-07-15 23:34:27 +0800

    初始化环境

---
+++ happy-mall-merchant-service/src/main/java/top/mall/happy/merchant/HappyMallMerchantServiceApplication.java
@@ -0,0 +1,13 @@
+package top.mall.happy.merchant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyMallMerchantServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyMallMerchantServiceApplication.class, args);
+    }
+
+}
