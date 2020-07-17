cd923af22f00bfed8864e8177845c0fbf4bd0223
xuhainan <859493231@qq.com>
2020-07-17 23:56:38 +0800

    add ucenter/getaway module

---
+++ happy-mall-getaway-service/src/main/java/top/mall/happy/getway/HappyMallGetwayServiceApplication.java
@@ -0,0 +1,13 @@
+package top.mall.happy.getway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyMallGetwayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyMallGetwayServiceApplication.class, args);
+    }
+
+}
---
+++ happy-mall-getaway-service/src/test/java/top/mall/happy/getway/HappyMallGetwayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package top.mall.happy.getway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyMallGetwayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
